335a6bf715aceb3bcbc65e416ddd6ef1bea1aba4
Ilya Zverev <zverik@textual.ru>
2018-04-19 00:17:09 +0300

    refactored a little

--- roles/get_veloroad/files/nik4wsgi_config.py
+++ roles/get_veloroad/files/nik4wsgi_config.py
@@ -1,15 +1,16 @@
 PYTHON = 'python3.5'
 NIK4 = '/opt/src/Nik4/nik4.py'
-STYLES = {
-    'veloroad': '/opt/styles/veloroad/veloroad-r.xml',
-    'veloroaden': '/opt/styles/veloroad/veloroad-en.xml',
-    'osm': '/opt/styles/osm-carto/osm-r.xml'
-}
-PARAMETRIC = True
-ROUTE = 'route'
-SCALE = 'scale'
-FORMATS = {
-    'png': 'image/png',
-    'svg': 'image/svg+xml',
-    'pdf': 'application/pdf'
+
+STYLES = [
+    ['veloroad', 'Veloroad Ru', '/opt/styles/veloroad/veloroad-r.xml', True],
+    ['veloroaden', 'Veloroad', '/opt/styles/veloroad/veloroad-en.xml', True],
+    ['osm', 'OSM-Carto', '/opt/styles/osm-carto/osm-r.xml', False],
+]
+TILES = {
+    'veloroad': ['http://tile.osmz.ru/veloroad/{z}/{x}/{y}.png',
+                 'Map &copy; OpenStreetMap | Tiles &copy Ilya Zverev'],
+    'veloroaden': ['http://tile.osmz.ru/veloroad/{z}/{x}/{y}.png',
+                   'Map &copy; OpenStreetMap | Tiles &copy Ilya Zverev'],
+    'osm': ['https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            'Map &copy; OpenStreetMap'],
 }
