cd2a6497361e508d898ad70116bcc33561f188cc
Ilya Zverev <zverik@textual.ru>
2018-05-02 15:29:18 +0300

    Hitrye forum install

---
+++ roles/hitrye/files/config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.2.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\postgres';
+$dbhost = 'localhost';
+$dbport = '';
+$dbname = 'hitrye';
+$dbuser = 'phpbb';
+$dbpasswd = '';
+$table_prefix = 'h_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);
