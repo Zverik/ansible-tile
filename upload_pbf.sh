cc5001a41fd807074fa6bc4583cc6c6bc9885d27
Ilya Zverev <zverik@textual.ru>
2018-05-03 23:52:45 +0300

    Upload_pbf works. Also fix an error with trim_osc

--- upload_pbf.sh
+++ upload_pbf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[ $# -lt 3 ] && echo "Usage: $0 <inventory> <planet.pbf> <state_idx>" && exit 1
-ansible-playbook upload_pbf.yml -i "$1" -e "pbf_file=$2" -e "pbf_state=$3"
+[ $# -lt 2 ] && echo "Usage: $0 <inventory> <planet.pbf> [<bounds.json>]" && exit 1
+ansible-playbook upload_pbf.yml -i "$1" -e "pbf=$2" ${3+-e "bounds=$3"}
