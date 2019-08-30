29a8e15b0549e5b5a2bfaf2d1c92cb5aef97d256
Ilya Zverev <zverik@textual.ru>
2019-08-31 00:19:09 +0300

    Update schedule builder

--- roles/schedules/files/update_schedules.sh
+++ roles/schedules/files/update_schedules.sh
@@ -5,26 +5,17 @@ WWW=/var/www/sotm
 HERE="$(cd "$(dirname "$0")"; pwd)"
 DATA="$HERE/sotm_schedules"
 VENV="$HERE/sc_venv"
+BASEURL="http://sotm.osmz.ru"
 
 mkdir -p "$DATA"
 wget -q -r -O "$DATA/sotmus2019.json" "https://sessionize.com/api/v2/sedalw8w/view/all"
 wget -q -r -O "$DATA/sotm2019.xml" "https://pretalx.com/sotm2019/schedule/export/schedule.xml"
 wget -q -r -O "$DATA/sotm2019-at.xml" "https://pretalx.com/sotm2019-at/schedule/export/schedule.xml"
-wget -q -r -O "$DATA/sotm2019-add.json" "https://wiki.openstreetmap.org/w/api.php?format=json&action=parse&prop=wikitext&page=State_of_the_Map_2019/Additional_Sessions"
+wget -q -r -O "$DATA/sotm2019-add.csv" 'https://docs.google.com/spreadsheets/d/1EO3aC3vF9dvb1wopT_cUHaDXqdGIrid_Kj59PZyzR_g/export?format=csv&id=0'
 wget -q -r -O "$DATA/hot2019.xml" "https://summit2019.hotosm.org/schedule.xml"
 sed -i -e 's#<track></track>#<track>Academic</track>#' "$DATA/sotm2019-at.xml"
 
-cd "$HERE/schedule-convert"
-if [ -x "$VENV/bin/python3" ]; then
-  PYTHON="$VENV/bin/python3"
-else
-  PYTHON=python3
-fi
-
-$PYTHON -m schedule_convert $DATA/sotmus2019.ini $DATA/sotmus2019.json -o $WWW/us19.xml
-$PYTHON -m schedule_convert $DATA/sotmus2019.ini $DATA/sotmus2019.json -f ical -o $WWW/us19.ics
-$PYTHON -m schedule_convert $DATA/hot2019.xml -o $WWW/hot19.xml
-$PYTHON -m schedule_convert $DATA/hot2019.xml -f ical -o $WWW/hot19.ics
-$PYTHON -m schedule_convert $DATA/sotm2019.xml $DATA/sotm2019-at.xml -o $WWW/19.xml
-$PYTHON -m schedule_convert $DATA/sotm2019.xml $DATA/sotm2019-at.xml -f ical -o $WWW/19.ics
-# $PYTHON -m schedule_convert $DATA/sotm2019.xml $DATA/sotm2019-at.xml $DATA/sotm2019-add.json -o $WWW/19.xml
+SC="$VENV/bin/schedule_convert"
+$SC $DATA/sotmus2019.ini $DATA/sotmus2019.json -l "$WWW" "$BASEURL/us19"
+$SC $DATA/hot2019.xml -l "$WWW" "$BASEURL/hot19"
+$SC $DATA/sotm2019.xml $DATA/sotm2019-at.xml $DATA/sotm2019-add.csv -l "$WWW" "$BASEURL/19"
