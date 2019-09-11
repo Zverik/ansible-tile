3ef2c67e403cb73016f6ed131f6b547f72684370
Ilya Zverev <zverik@textual.ru>
2019-09-11 09:04:07 +0300

    Update schedule generation

--- roles/schedules/files/update_schedules.sh
+++ roles/schedules/files/update_schedules.sh
@@ -8,7 +8,8 @@ VENV="$HERE/sc_venv"
 BASEURL="http://sotm.osmz.ru"
 
 mkdir -p "$DATA"
-wget -q -r -O "$DATA/sotmus2019.json" "https://sessionize.com/api/v2/sedalw8w/view/all"
+# wget -q -r -O "$DATA/sotmus2019.json" "https://sessionize.com/api/v2/sedalw8w/view/all"
+# wget -q -r -O "$DATA/sotmus2019-add.csv" 'https://docs.google.com/spreadsheets/d/1GmVqN7bSRy1Sn3tPiuLpuNgFm8gKffsZeCRK7TWqMAA/export?format=csv&id=0'
 wget -q -r -O "$DATA/sotm2019.xml" "https://pretalx.com/sotm2019/schedule/export/schedule.xml"
 wget -q -r -O "$DATA/sotm2019-at.xml" "https://pretalx.com/sotm2019-at/schedule/export/schedule.xml"
 wget -q -r -O "$DATA/sotm2019-add.csv" 'https://docs.google.com/spreadsheets/d/1EO3aC3vF9dvb1wopT_cUHaDXqdGIrid_Kj59PZyzR_g/export?format=csv&id=0'
@@ -16,6 +17,6 @@ wget -q -r -O "$DATA/hot2019.xml" "https://summit2019.hotosm.org/schedule.xml"
 sed -i -e 's#<track></track>#<track>Academic</track>#' "$DATA/sotm2019-at.xml"
 
 SC="$VENV/bin/schedule_convert"
-$SC $DATA/sotmus2019.ini $DATA/sotmus2019.json -l "$WWW" "$BASEURL/us19"
+# $SC $DATA/sotmus2019.ini $DATA/sotmus2019.json $DATA/sotmus2019-add.csv -l "$WWW" "$BASEURL/us19"
 $SC $DATA/hot2019.xml -l "$WWW" "$BASEURL/hot19"
-$SC $DATA/sotm2019.xml $DATA/sotm2019-at.xml $DATA/sotm2019-add.csv -l "$WWW" "$BASEURL/19"
+$SC $DATA/sotm2019.ini $DATA/sotm2019.xml $DATA/sotm2019-at.xml $DATA/sotm2019-add.csv -l "$WWW" "$BASEURL/19"
