3f24d1f4e6b09b2fdc07664c58bfdf80986d586d
Ilya Zverev <zverik@textual.ru>
2020-02-26 10:27:34 +0300

    Add baltic git

--- roles/schedules/files/update_schedules.sh
+++ roles/schedules/files/update_schedules.sh
@@ -10,13 +10,15 @@ BASEURL="http://sotm.osmz.ru"
 mkdir -p "$DATA"
 # wget -q -r -O "$DATA/sotmus2019.json" "https://sessionize.com/api/v2/sedalw8w/view/all"
 # wget -q -r -O "$DATA/sotmus2019-add.csv" 'https://docs.google.com/spreadsheets/d/1GmVqN7bSRy1Sn3tPiuLpuNgFm8gKffsZeCRK7TWqMAA/export?format=csv&id=0'
-wget -q -r -O "$DATA/sotm2019.xml" "https://pretalx.com/sotm2019/schedule/export/schedule.xml"
-wget -q -r -O "$DATA/sotm2019-at.xml" "https://pretalx.com/sotm2019-at/schedule/export/schedule.xml"
-wget -q -r -O "$DATA/sotm2019-add.csv" 'https://docs.google.com/spreadsheets/d/1EO3aC3vF9dvb1wopT_cUHaDXqdGIrid_Kj59PZyzR_g/export?format=csv&id=0'
-wget -q -r -O "$DATA/hot2019.xml" "https://summit2019.hotosm.org/schedule.xml"
-sed -i -e 's#<track></track>#<track>Academic</track>#' "$DATA/sotm2019-at.xml"
+# wget -q -r -O "$DATA/sotm2019.xml" "https://pretalx.com/sotm2019/schedule/export/schedule.xml"
+# wget -q -r -O "$DATA/sotm2019-at.xml" "https://pretalx.com/sotm2019-at/schedule/export/schedule.xml"
+# wget -q -r -O "$DATA/sotm2019-add.csv" 'https://docs.google.com/spreadsheets/d/1EO3aC3vF9dvb1wopT_cUHaDXqdGIrid_Kj59PZyzR_g/export?format=csv&id=0'
+# wget -q -r -O "$DATA/hot2019.xml" "https://summit2019.hotosm.org/schedule.xml"
+# sed -i -e 's#<track></track>#<track>Academic</track>#' "$DATA/sotm2019-at.xml"
+wget -q -r -O "$DATA/baltic20.csv" 'https://docs.google.com/spreadsheets/d/1KFyBt57l-wUKdyTZNgLMHHV9ZwjRX74dNOIG93Y0I5A/export?format=csv&id=0'
 
 SC="$VENV/bin/schedule_convert"
 # $SC $DATA/sotmus2019.ini $DATA/sotmus2019.json $DATA/sotmus2019-add.csv -l "$WWW" "$BASEURL/us19"
-$SC $DATA/hot2019.xml -l "$WWW" "$BASEURL/hot19"
-$SC $DATA/sotm2019.ini $DATA/sotm2019.xml $DATA/sotm2019-at.xml $DATA/sotm2019-add.csv -l "$WWW" "$BASEURL/19"
+# $SC $DATA/hot2019.ini $DATA/hot2019.xml -l "$WWW" "$BASEURL/hot19"
+# $SC $DATA/sotm2019.ini $DATA/sotm2019.xml $DATA/sotm2019-at.xml $DATA/sotm2019-add.csv -l "$WWW" "$BASEURL/19"
+$SC $DATA/balticgit20.ini $DATA/baltic20.csv -l "$WWW" "$BASEURL/baltic20"
