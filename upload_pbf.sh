7f732edc80f532e3b08f84872326a93367f089fd
Ilya Zverev <zverik@textual.ru>
2020-03-04 00:37:22 +0300

    Tested upload_pbf

--- upload_pbf.sh
+++ upload_pbf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[ $# -lt 2 ] && echo "Usage: $0 <planet.pbf> [<bounds.json>]" && exit 1
-ansible-playbook upload_pbf.yml -i hosts -e "pbf=$2" ${3+-e "bounds=$3"}
+[ $# -lt 1 ] && echo "Usage: $0 <planet.pbf> [<bounds.json>]" && exit 1
+ansible-playbook upload_pbf.yml -i hosts -e "pbf=$1" ${2+-e "bounds=$2"}
