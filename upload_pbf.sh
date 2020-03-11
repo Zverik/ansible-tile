d31220e9f2ac6165a7c9ad80f05af3ff295414e7
Ilya Zverev <zverik@textual.ru>
2020-03-11 09:20:02 +0300

    Update uploading pbf

--- upload_pbf.sh
+++ upload_pbf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 [ $# -lt 1 ] && echo "Usage: $0 <planet.pbf> [<bounds.json>]" && exit 1
-ansible-playbook upload_pbf.yml -i hosts -e "pbf=$1" ${2+-e "bounds=$2"}
+#ansible-playbook upload_pbf.yml -i hosts -e "pbf=$1" ${2+-e "bounds=$2"}
+ansible-playbook upload_pbf.yml -i hosts -e "pbf=$1" ${2+-e "bounds=$2"} --skip-tags osm2pgsql
